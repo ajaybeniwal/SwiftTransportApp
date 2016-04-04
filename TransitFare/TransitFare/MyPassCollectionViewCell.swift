f709fc7d74c10858480df35fe4852c3fc82f1f5a
Ajay Singh <ajaybeniwal203@gmail.com>
2016-04-04 16:06:11 +0800

    Changes for adding ui collection view cell

---
+++ TransitFare/TransitFare/MyPassCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  MyPassCollectionViewCell.swift
+//  TransitFare
+//
+//  Created by Ajay Singh on 4/4/16.
+//  Copyright © 2016 ajaybeniwal203. All rights reserved.
+//
+
+import UIKit
+
+class MyPassCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var barCodeImage: UIImageView!
+}
---
+++ TransitFare/TransitFare/MyPassCollectionViewController.swift
@@ -0,0 +1,88 @@
+//
+//  MyPassCollectionViewController.swift
+//  TransitFare
+//
+//  Created by Ajay Singh on 4/4/16.
+//  Copyright © 2016 ajaybeniwal203. All rights reserved.
+//
+
+import UIKit
+import Kingfisher
+
+private let reuseIdentifier = "MyPassCell"
+
+class MyPassCollectionViewController: UICollectionViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+   
+    // MARK: UICollectionViewDataSource
+
+    override func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+
+    override func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of items
+        return 2
+    }
+
+    override func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier(reuseIdentifier, forIndexPath: indexPath) as! MyPassCollectionViewCell
+        cell.barCodeImage.kf_setImageWithURL(NSURL(string: "https://chart.googleapis.com/chart?cht=qr&chl=9871116244&choe=UTF-8&chs=280x280")!)
+       
+        cell.layer.cornerRadius = 4
+        cell.layer.masksToBounds = false
+        cell.layer.shadowColor = UIColor.blackColor().CGColor
+        cell.layer.shadowOffset = CGSizeMake(2,2);
+        cell.layer.shadowOpacity = 0.5
+        cell.layer.shadowRadius = 4
+        
+        
+        // Configure the cell
+    
+        return cell
+    }
+
+    // MARK: UICollectionViewDelegate
+
+    /*
+    // Uncomment this method to specify if the specified item should be highlighted during tracking
+    override func collectionView(collectionView: UICollectionView, shouldHighlightItemAtIndexPath indexPath: NSIndexPath) -> Bool {
+        return true
+    }
+    */
+
+    /*
+    // Uncomment this method to specify if the specified item should be selected
+    override func collectionView(collectionView: UICollectionView, shouldSelectItemAtIndexPath indexPath: NSIndexPath) -> Bool {
+        return true
+    }
+    */
+
+    /*
+    // Uncomment these methods to specify if an action menu should be displayed for the specified item, and react to actions performed on the item
+    override func collectionView(collectionView: UICollectionView, shouldShowMenuForItemAtIndexPath indexPath: NSIndexPath) -> Bool {
+        return false
+    }
+
+    override func collectionView(collectionView: UICollectionView, canPerformAction action: Selector, forItemAtIndexPath indexPath: NSIndexPath, withSender sender: AnyObject?) -> Bool {
+        return false
+    }
+
+    override func collectionView(collectionView: UICollectionView, performAction action: Selector, forItemAtIndexPath indexPath: NSIndexPath, withSender sender: AnyObject?) {
+    
+    }
+    */
+
+}
